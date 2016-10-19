aca4d07d1e4958ebf969a5eafa3eacba1218a20c
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-10-19 13:32:38 +0800

    加速度传感器 完成 (先让我睡上一觉...)

--- app/src/main/java/com/lulu/weichatshake/MainActivity.java
+++ app/src/main/java/com/lulu/weichatshake/MainActivity.java
@@ -1,13 +1,79 @@
 package com.lulu.weichatshake;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometerSensor;
+
+    //记录摇动状态
+    private boolean isShake = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //获取 SensorManager 负责管理传感器
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
+
+        if (mSensorManager != null) {
+            //获取加速度传感器
+            mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (mAccelerometerSensor != null) {
+                mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_UI);
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        //不要忘记在Activity销毁时注销 mAccelerometerSensor
+        if (mSensorManager != null) {
+            mSensorManager.unregisterListener(this);
+        }
+        super.onDestroy();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // SensorEventListener回调方法
+    ///////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int type = event.sensor.getType();
+
+        if (type == Sensor.TYPE_ACCELEROMETER) {
+            //获取三个方向值
+            float[] values = event.values;
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            if ((Math.abs(x) > 17 || Math.abs(y) > 17 || Math
+                    .abs(z) > 17) && !isShake) {
+                isShake = true;
+                Toast.makeText(this, "摇动", Toast.LENGTH_SHORT).show();
+
+                // TODO: 2016/10/19 实现摇动逻辑
+
+            }
+
+        }
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
